9f5abfac21df0ed84d073a6fbe5d4cea58279900
Алексей <112748030+AlexeyRF@users.noreply.github.com>
2026-05-13 10:00:55 +0500

    Add 'requests' package to installation script

--- install.bat
+++ install.bat
@@ -1,1 +1,1 @@
-pip install pywin32 pyqt5 psutil cryptography==46.0.5 customtkinter==5.2.2 Pillow==12.1.1 pystray==0.19.5 pyperclip==1.9.0
+pip install pywin32 pyqt5 psutil requests cryptography==46.0.5 customtkinter==5.2.2 Pillow==12.1.1 pystray==0.19.5 pyperclip==1.9.0
